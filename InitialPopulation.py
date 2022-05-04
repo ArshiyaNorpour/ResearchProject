1c3ec3bd67255401d3867acbe7da0d4d4888e498
ArshiyaNorpour <arshiyanorpour@gmail.com>
2022-05-04 17:02:21 +0430

    Bug fix3d

--- InitialPopulation.py
+++ InitialPopulation.py
@@ -5,18 +5,24 @@ operands = ["num", "x"]
 def InitialPopulation():
     strAns = ""
     strWeigh = random.randint(1,10)
+    varAdd = True
+    operatorAdd = True
     for i in range(strWeigh):
         if i == 0 or i == (strWeigh-1):
-            if random.randint(0,1) == 0:
-                strAns += str(random.randint(1, 100))
+            if random.randint(0,1) == 0 or varAdd == False:
+                strAns += str(random.randint(1, 9))
+                varAdd = True
             else:
                 strAns += "x"
+                varAdd = False
         else:
-            if random.randint(0,1) == 0:
+            if random.randint(0,1) == 0 and operatorAdd == True:
                 strAns += operators[random.randint(0,3)]
+                operatorAdd = False
             else:
-                if random.randint(0,1) == 0:
-                    strAns += str(random.randint(1, 100))
+                if random.randint(0,1) == 0 or varAdd == False:
+                    strAns += str(random.randint(1, 9))
                 else:
                     strAns += "x"
+                    varAdd = False
     return strAns
